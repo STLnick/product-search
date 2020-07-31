5bedcc5a112c7f66776ddc542e997d000a0b0bdb
Nick Ray <nickrray@gmail.com>
2020-07-31 16:44:35 -0500

    :construction: Setup state and render

--- src/components/ProductSearchDisplay/index.js
+++ src/components/ProductSearchDisplay/index.js
@@ -4,3 +4,19 @@ import { ProductTable } from './ProductTable'
 import { SearchBar } from './SearchBar'
 
 import api from 'api'
+
+export class ProductSearchDisplay extends React.Component {
+  state = {
+    filteredProducts: [],
+    products: [],
+    searchText: ''
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar handler={this.handleSearchTextChange} text={this.state.searchText} />
+        <ProductTable products={this.state.filteredProducts} />
+      </div>
+    )
+  }
+}
