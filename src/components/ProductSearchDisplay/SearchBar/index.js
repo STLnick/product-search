a1f81274ed19a7dd73e1deb7ef161a0f702712c8
Nick Ray <nickrray@gmail.com>
2020-07-31 16:45:41 -0500

    :sparkles: Setup SearchBar component

--- src/components/ProductSearchDisplay/SearchBar/index.js
+++ src/components/ProductSearchDisplay/SearchBar/index.js
@@ -1,2 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+
+export const SearchBar = ({ handler, text }) => {
+
+  return (
+    <div className="search-container">
+      <label htmlFor="search">Search</label>
+      <input id="search" type="text" onChange={handler} placeholder="Search..." value={text} />
+    </div>
+  )
+}
+
+SearchBar.propTypes = {
+  handler: PropTypes.func,
+  text: PropTypes.string
+}
