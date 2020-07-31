70b9d24e2a14480feff8ec1d4dbfb872dc1ff52d
Nick Ray <nickrray@gmail.com>
2020-07-31 16:46:22 -0500

    :rotating_light: Utilize PropTypes

--- src/components/ProductSearchDisplay/ProductTable/ProductRow/index.js
+++ src/components/ProductSearchDisplay/ProductTable/ProductRow/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export const ProductRow = ({ products }) => products.map(product => (
   <tr key={product.name}>
     <td>{product.name}</td>
     <td>{product.price}</td>
   </tr>
-))
+)
+
+ProductRow.propTypes = {
+  name: PropTypes.string,
+  price: PropTypes.string
+}
