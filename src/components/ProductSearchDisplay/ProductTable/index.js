041b4c241d89f9b788ef26b94bd960efcb1720ab
Nick Ray <nickrray@gmail.com>
2020-07-31 16:47:29 -0500

    :construction: Start setting up ProductTable component

--- src/components/ProductSearchDisplay/ProductTable/index.js
+++ src/components/ProductSearchDisplay/ProductTable/index.js
@@ -3,3 +3,23 @@ import PropTypes from 'prop-types'
 
 import { ProductCategoryRow } from './ProductCategoryRow'
 import { ProductRow } from './ProductRow'
+
+export const ProductTable = ({ products }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {renderTableBody()}
+      </tbody>
+    </table>
+  )
+}
+
+ProductTable.propTypes = {
+  products: PropTypes.array
+}
