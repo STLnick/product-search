948b11bd0195fbaf7b8b5828147b537bd935bf31
Nick Ray <nickrray@gmail.com>
2020-08-03 13:53:52 -0500

    :lipstick: Style checkbox input and label and increase wrapper height

--- src/components/ProductSearchDisplay/SearchBar/index.js
+++ src/components/ProductSearchDisplay/SearchBar/index.js
@@ -9,8 +9,10 @@ export const SearchBar = ({ checkboxHandler, textHandler, text }) => {
     <div className="search-container">
       <label className="screen-reader-text" htmlFor="search">Search</label>
       <input id="search" type="text" onChange={textHandler} placeholder="Search..." value={text} />
-      <label htmlFor="stocked">In Stock Items Only</label>
-      <input id="stocked" type="checkbox" onChange={checkboxHandler} />
+      <div className="stocked-container flex flex--align-center flex--justify-center">
+        <label htmlFor="stocked">In Stock Items Only</label>
+        <input id="stocked" type="checkbox" onChange={checkboxHandler} />
+      </div>
     </div>
   )
 }
