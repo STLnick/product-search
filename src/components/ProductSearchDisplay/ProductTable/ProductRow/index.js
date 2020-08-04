6c3f6d104b647a8a63e1c31935c15287b0e4bda1
Nick Ray <nickrray@gmail.com>
2020-08-04 13:12:55 -0500

    :sparkles: Pass class name in render of ProductRow to make red if out of stock

--- src/components/ProductSearchDisplay/ProductTable/ProductRow/index.js
+++ src/components/ProductSearchDisplay/ProductTable/ProductRow/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export const ProductRow = ({ name, price }) => (
-  <tr>
+export const ProductRow = ({ className, name, price }) => (
+  <tr className={className}>
     <td>{name}</td>
     <td>{price}</td>
   </tr>
 )
 
 ProductRow.propTypes = {
+  className: PropTypes.string,
   name: PropTypes.string,
   price: PropTypes.string
 }
