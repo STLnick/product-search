23a769a8b2fc888e855f207885a1eb6f4d21eb5b
Nick Ray <nickrray@gmail.com>
2020-08-04 13:29:50 -0500

    :recycle: Refactor to utilize new css class to make red when out of stock

--- src/components/ProductSearchDisplay/ProductTable/ProductRow/index.js
+++ src/components/ProductSearchDisplay/ProductTable/ProductRow/index.js
@@ -1,19 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export const ProductRow = ({ name, price, inStock }) => inStock ?
-  (
-    <tr>
-      <td>{name}</td>
-      <td>{price}</td>
-    </tr>
-  ) : (
-    <tr style={{ color: 'red' }}>
-      <td>{name}</td>
-      <td>{price}</td>
-    </tr>
-  )
+import './ProductRow.css'
 
+export const ProductRow = ({ name, price, inStock }) => (
+  <tr>
+    <td className={inStock ? '' : 'out-of-stock'}>{name}</td>
+    <td>{price}</td>
+  </tr>
+)
 
 ProductRow.propTypes = {
   name: PropTypes.string,
